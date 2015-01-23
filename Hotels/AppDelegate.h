03976fcf392134c2245d659390a1e430607ce35c
Spencer Morris <spencer@ticketscalpr.com>
2015-01-23 13:40:32 -0600

    Initial Commit

---
+++ Hotels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hotels
+//
+//  Created by Spencer Morris on 1/23/15.
+//  Copyright (c) 2015 SpencerMorris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hotels/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hotels
+//
+//  Created by Spencer Morris on 1/23/15.
+//  Copyright (c) 2015 SpencerMorris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
