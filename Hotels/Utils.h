b01b555e3d79c59dbb637efd6e7dea243f8e519f
Spencer Morris <spencer@ticketscalpr.com>
2015-01-24 13:15:59 -0600

    Create utility class

---
+++ Hotels/Utils.h
@@ -0,0 +1,13 @@
+//
+//  Utils.h
+//  Hotels
+//
+//  Created by Spencer Morris on 1/24/15.
+//  Copyright (c) 2015 SpencerMorris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utils : NSObject
+
+@end
---
+++ Hotels/Utils.m
@@ -0,0 +1,13 @@
+//
+//  Utils.m
+//  Hotels
+//
+//  Created by Spencer Morris on 1/24/15.
+//  Copyright (c) 2015 SpencerMorris. All rights reserved.
+//
+
+#import "Utils.h"
+
+@implementation Utils
+
+@end
